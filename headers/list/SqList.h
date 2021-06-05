19ba497940b3589f69033be46afdd12bb6549b10
scq355 <nwpuscq355@163.com>
2021-06-05 10:23:30 +0800

    .ignore

---
+++ headers/list/SqList.h
@@ -0,0 +1,8 @@
+//
+// Created by Jimmy on 2021/6/3.
+//
+
+#ifndef DATASTRUCTURE_SQLIST_H
+#define DATASTRUCTURE_SQLIST_H
+
+#endif //DATASTRUCTURE_SQLIST_H
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "headers/list/CLinkList.h"
+#include "headers/tree/BiTree.h"
+#include "headers/stack/LinkStack.h"
+#include "headers/queue/LinkQueue.h"
+#include "headers/stack/SqStack.h"
+#include "headers/queue/SqQueue.h"
+#include "headers/list/LinkList.h"
+
+int main() {
+//    cLinkListOperate();
+//    cLinkListHeadOperate();
+//    biTreeOperate();
+//    linkStackOperate();
+//    linkQueueOperate();
+//    sqStackOperate();
+//    sqQueueOperate();
+
+//    linkListOperate();
+//    headOperate();
+//    tailOperate();
+//    mergeLinkListOperate();
+
+
+}
